1dc2ac708b6c5f05180c516daf95b6ad87e8462f
cp2004 <31997505+cp2004@users.noreply.github.com>
2020-11-14 17:42:20 +0000

    :sparkles: New plugin with versioneer, pre-commit, pre-release

--- octoprint_autologin_config/__init__.py
+++ octoprint_autologin_config/__init__.py
@@ -1,79 +1,69 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
 from __future__ import absolute_import
-
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
 
 import octoprint.plugin
 
-class Autologin_configPlugin(octoprint.plugin.SettingsPlugin,
-                             octoprint.plugin.AssetPlugin,
-                             octoprint.plugin.TemplatePlugin):
+from ._version import get_versions
 
-	##~~ SettingsPlugin mixin
-
-	def get_settings_defaults(self):
-		return dict(
-			# put your plugin's default settings here
-		)
-
-	##~~ AssetPlugin mixin
-
-	def get_assets(self):
-		# Define your plugin's asset files to automatically include in the
-		# core UI here.
-		return dict(
-			js=["js/autologin_config.js"],
-			css=["css/autologin_config.css"],
-			less=["less/autologin_config.less"]
-		)
-
-	##~~ Softwareupdate hook
-
-	def get_update_information(self):
-		# Define the configuration for your plugin to use with the Software Update
-		# Plugin here. See https://docs.octoprint.org/en/master/bundledplugins/softwareupdate.html
-		# for details.
-		return dict(
-			autologin_config=dict(
-				displayName="Autologin_config Plugin",
-				displayVersion=self._plugin_version,
-
-				# version check: github repository
-				type="github_release",
-				user="cp2004",
-				repo="OctoPrint-AutoLoginConfig",
-				current=self._plugin_version,
-
-				# update method: pip
-				pip="https://github.com/cp2004/OctoPrint-AutoLoginConfig/archive/{target_version}.zip"
-			)
-		)
+__version__ = get_versions()["version"]
+del get_versions
 
 
-# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
-# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
-# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
+class AutologinConfigPlugin(
+    octoprint.plugin.AssetPlugin,
+    octoprint.plugin.TemplatePlugin,
+):
+
+    # TemplatePlugin mixin
+    def get_template_configs(self):
+        return [{"type": "settings", "custom_bindings": False}]
+
+    # AssetPlugin mixin
+    def get_assets(self):
+        return {
+            "js": ["js/autologin_config.js"],
+            "css": ["css/autologin_config.css"],
+        }
+
+    # Softwareupdate hook
+    def get_update_information(self):
+        return {
+            "autologin_config": {
+                "displayName": "Autologin Config",
+                "displayVersion": self._plugin_version,
+                # version check: github repository
+                "type": "github_release",
+                "user": "cp2004",
+                "repo": "OctoPrint-AutoLoginConfig",
+                "current": self._plugin_version,
+                "stable_branch": {
+                    "name": "Stable",
+                    "branch": "master",
+                    "comittish": ["main"],
+                },
+                "prerelease_branches": [
+                    {
+                        "name": "Release Candidate",
+                        "branch": "pre-release",
+                        "comittish": ["pre-release", "main"],
+                    }
+                ],
+                # update method: pip
+                "pip": "https://github.com/cp2004/OctoPrint-AutoLoginConfig/archive/{target_version}.zip",
+            }
+        }
+
+
 __plugin_name__ = "Autologin_config Plugin"
+__plugin_version__ = __version__
+__plugin_pythoncompat__ = ">=2.7,<4"  # python 2 and 3
 
-# Starting with OctoPrint 1.4.0 OctoPrint will also support to run under Python 3 in addition to the deprecated
-# Python 2. New plugins should make sure to run under both versions for now. Uncomment one of the following
-# compatibility flags according to what Python versions your plugin supports!
-#__plugin_pythoncompat__ = ">=2.7,<3" # only python 2
-#__plugin_pythoncompat__ = ">=3,<4" # only python 3
-#__plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
 
 def __plugin_load__():
-	global __plugin_implementation__
-	__plugin_implementation__ = Autologin_configPlugin()
+    global __plugin_implementation__
+    __plugin_implementation__ = AutologinConfigPlugin()
 
-	global __plugin_hooks__
-	__plugin_hooks__ = {
-		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
-	}
-
+    global __plugin_hooks__
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+    }
--- octoprint_autologin_config/static/js/autologin_config.js
+++ octoprint_autologin_config/static/js/autologin_config.js
@@ -4,26 +4,16 @@
  * Author: Charlie Powell
  * License: AGPLv3
  */
-$(function() {
+$(function () {
     function Autologin_configViewModel(parameters) {
         var self = this;
-
-        // assign the injected parameters, e.g.:
-        // self.loginStateViewModel = parameters[0];
-        // self.settingsViewModel = parameters[1];
-
-        // TODO: Implement your plugin's view model here.
+        self.settingsViewModel = parameters[1];
     }
 
-    /* view model class, parameters for constructor, container to bind to
-     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
-     * and a full list of the available options.
-     */
     OCTOPRINT_VIEWMODELS.push({
         construct: Autologin_configViewModel,
-        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
-        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
+        dependencies: [],
         // Elements to bind to, e.g. #settings_plugin_autologin_config, #tab_plugin_autologin_config, ...
-        elements: [ /* ... */ ]
+        elements: [""],
     });
 });
