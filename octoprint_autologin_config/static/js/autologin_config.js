OctoPrint/OctoPrint-AutoLoginConfig

commit 2f11f523fa0468296ae754fe1bce21762d88b1a0

:sparkles: Previously added fields are editable

octoprint_autologin_config/static/js/autologin_config.js
@@ -63,11 +63,14 @@ $(function () {
             if (!self.check_admin()) {
                 return;
             }
-
+            var localNetworksArray = [];
+            _.each(self.localNetworks(), function (network) {
+                localNetworksArray.push(network());
+            });
             OctoPrint.simpleApiCommand("autologin_config", "save_config", {
                 enabled: self.enabled(),
                 loginAs: self.loginAs(),
-                localNetworks: self.localNetworks(),
+                localNetworks: localNetworksArray,
             }).done(self.on_api_repsonse);
         };
 
@@ -92,7 +95,10 @@ $(function () {
                 self.loginAs(response.loginAs);
             }
             if (response.localNetworks) {
-                self.localNetworks(response.localNetworks);
+                self.localNetworks([]);
+                _.each(response.localNetworks, function (network) {
+                    self.localNetworks.push(ko.observable(network));
+                });
             }
         };
     }

octoprint_autologin_config/templates/autologin_config_settings.jinja2
@@ -59,7 +59,7 @@
             </div>
             <!-- ko foreach: localNetworks() -->
             <div class="input-append">
-                <input type="text" class="network-input" data-bind="value: $data">
+                <input type="text" class="network-input" data-bind="value: $rawData">
                 <button class="btn btn-danger" data-bind="click: $parent.removeLocalNetwork"><i class="fa fa-trash-alt"></i></button>
             </div>
             <!-- /ko -->

commit c78a469aa5af8804a7156289349a8e401fe62a95

:bug: Fix adding or deleting networks
Fixes changes introduced in 2f11f523fa0468296ae754fe1bce21762d88b1a0

octoprint_autologin_config/static/js/autologin_config.js
@@ -42,6 +42,7 @@ $(function () {
                 return;
             }
             self.localNetworks.unshift(self.newLocalNetwork());
+            self.localNetworks.unshift(ko.observable(self.newLocalNetwork()));
             self.newLocalNetwork("");
         };
 

octoprint_autologin_config/templates/autologin_config_settings.jinja2
@@ -60,7 +60,7 @@
             <!-- ko foreach: localNetworks() -->
             <div class="input-append">
                 <input type="text" class="network-input" data-bind="value: $rawData">
-                <button class="btn btn-danger" data-bind="click: $parent.removeLocalNetwork"><i class="fa fa-trash-alt"></i></button>
+                <button class="btn btn-danger" data-bind="click: function() { $parent.removeLocalNetwork($rawData) }"><i class="fa fa-trash-alt"></i></button>
             </div>
             <!-- /ko -->
         </div>